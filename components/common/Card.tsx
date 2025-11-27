e22e20dfa5eb1292d1e60e35a91ebef351165cf6
Mai-Elhajeen <maielhajeen2001>
2025-11-27 23:14:16 +0200

    feat(update): handel description in card, add variant param to control in card view and style, add index param for control direction content card (left, right)

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -12,20 +12,26 @@ export default function Card({
   size,
   avatar,
   name,
-}: CardProps) {
+  description,
+  variant = "grid",
+  index, 
+}: CardProps & { variant?: "grid" | "list"; index?: number }) {
+  const isEven = index !== undefined && index % 2 ===1;
+  const listClasses = isEven ? "flex-row-reverse" : "flex-row";
   return (
-    <div className="w-[370px] bg-white rounded-lg shadow-md">
+    <div className={variant === "grid" ? "w-[370px] bg-white rounded-lg shadow-md" : `flex w-full bg-white rounded-xl shadow-md overflow-hidden h-[270px] ${listClasses}`}>
       <Image
         src={img}
         alt={title}
-        className="w-full h-[180px] object-cover rounded-t-lg"
+        className={variant === "grid" ? "w-full h-[180px] object-cover rounded-t-lg" : "h-full w-[40%] object-cover"}
         width={300}
         height={180}
       />
-      <div className="p-4 flex flex-col gap-2">
-        <div className="font-medium text-lg">{title}</div>
-        <div className="font-semibold text-lg text-primary-light">${price}</div>
-        <div className="flex items-center gap-4 text-gray-500 text-sm">
+      <div className={variant === "grid" ? "p-4 flex flex-col gap-2" : "p-5 flex flex-col justify-between w-[60%]"}>
+        <div className={variant === "grid" ? "font-medium text-lg text-primary-light" : "font-medium text-lg text-primary-light order-2"}>{title}</div>
+        <div className={variant === "grid" ? "hidden" : "font-normal text-sm text-gray-500 block order-3"}>{description}</div>
+        <div className={variant === "grid" ? "font-bold text-xl text-primary-light" : "font-bold text-xl text-primary-light order-4"}>${price}</div>
+        <div className={variant === "grid" ? "flex items-center gap-4 text-gray-500 text-sm" : "flex items-center gap-6 text-gray-600 text-sm mt-2 hidden"}>
           <div className="flex gap-1.5 items-center">
             <CarFront size={16} />
             <span>{garag}</span>
@@ -39,8 +45,8 @@ export default function Card({
             <span>{size}ft</span>
           </div>
         </div>
-        <hr className="border-gray-200 my-2" />
-        <div className="flex justify-between items-center">
+        {variant === "grid" && <hr className="border-gray-200 my-2" />}
+        <div className={variant === "grid" ? "flex justify-between items-center" : "flex justify-between items-center mt-4 order-1"}>
           <Avatar src={avatar} name={name} size={30} />
           <div className="flex gap-2">
             <div className="cursor-pointer bg-primary-extra-light text-primary-light p-1 rounded-sm">
