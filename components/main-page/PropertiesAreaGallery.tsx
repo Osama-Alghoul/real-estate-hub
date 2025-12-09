617c71787e18000ad35f1be17b1d52a6b15dc3af
OsssGhoul <osamalghoul2@gmail.com>
2025-12-09 14:47:40 +0200

    style(home): add hover effect to gallery section

--- components/main-page/PropertiesAreaGallery.tsx
+++ components/main-page/PropertiesAreaGallery.tsx
@@ -3,9 +3,9 @@ import AreaGallery from "./AreaGallery";
 
 export default function PropertiesAreaGallery() {
   return (
-    <section className="flex flex-col items-center justify-center">
+    <section className="py-16 md:py-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <SectionTitle
-        title="Propertice by Area"
+        title="Properties by Area"
         description="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin sodales ultrices nulla blandit volutpat."
       />
       <AreaGallery />
