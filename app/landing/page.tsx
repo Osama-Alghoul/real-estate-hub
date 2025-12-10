363e379d8c31551e8f6bc4e000321588b946f046
Mai-Elhajeen <maielhajeen2001>
2025-12-10 17:17:09 +0200

    related resolve conflict

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,9 +1,21 @@
 import Hero from "../../components/main-page/hero";
+import Footer from "@/components/layout/footer";
+import Cards from "@/components/main-page/cards";
+import CTA from "@/components/main-page/CTA";
+import PlatformFeatures from "@/components/main-page/platformFeatures";
+import PropertiesAreaGallery from "@/components/main-page/PropertiesAreaGallery";
+import Slider from "@/components/main-page/slider";
 
 export default function HomeLandingPage() {
-    return (
-        <>
-            <Hero />
-        </>
-    );
+  return (
+    <>
+      <Hero />
+      <PropertiesAreaGallery />
+      <PlatformFeatures />
+      <Cards />
+      <Slider />
+      <CTA />
+      <Footer />
+    </>
+  );
 }
